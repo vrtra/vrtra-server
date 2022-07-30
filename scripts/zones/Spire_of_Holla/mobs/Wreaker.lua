d9a2ba9c47ad39f33f864bb39eb144265302ac07
vrtra <ffxi.vrtra@gmail.com>
2022-07-30 13:23:30 -0500

    ugh

--- scripts/zones/Spire_of_Holla/mobs/Wreaker.lua
+++ scripts/zones/Spire_of_Holla/mobs/Wreaker.lua
@@ -1,26 +1,37 @@
 -----------------------------------
 -- Area: Spire of Holla
---  Mob: Progenerator
+--  Mob: Wreaker
+-----------------------------------
+mixins = {require("scripts/mixins/families/empty_terroanima")}
 -----------------------------------
 local entity = {}
 
-entity.onMobInitialize = function(mob)
-end
-
 entity.onMobSpawn = function(mob)
-    mob:setLocalVar("maxBabies", 4)
+    mob:setMod(xi.mod.DOUBLE_ATTACK, 20)
+    mob:addMod(xi.mod.DEFP, 35)
 end
 
-entity.onMobEngaged = function(mob, target)
-end
-
-entity.onMobWeaponSkill = function(target, mob, skill)
+entity.onMobWeaponSkillPrepare = function(mob, target)
+    local roll = math.random()
+    if mob:getHPP() <= 25 then
+        if roll <= 0.25 then
+            return 1252 -- Shadow_spread
+        else
+            return 1248 -- Trinary_absorption
+        end
+    end
 end
 
 entity.onMobFight = function(mob, target)
+    if mob:getTP() >= 2000 then
+        mob:useMobAbility()
+    end
+    if mob:getHPP() <= 35 then
+       mob:setMod(xi.mod.STORETP, 250)
+    end
 end
-
 entity.onMobDeath = function(mob, player, isKiller)
+	player:setCharVar("holla_win", 1)
 end
 
 return entity
